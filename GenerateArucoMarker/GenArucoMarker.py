44f98e655323315279b1090e1091baa28917f6c3
Kyle <kyle.elsalhi@outlook.com>
2017-04-28 17:22:57 -0700

    Updating comments

--- GenerateArucoMarker/GenArucoMarker.py
+++ GenerateArucoMarker/GenArucoMarker.py
@@ -4,11 +4,10 @@ import cv2.aruco as aruco
 
 # Select type of aruco marker (size)
 aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_1000)
-print(aruco_dict)
-# second param is ID number
-# last param is total image size
 
 # Create an image from the marker
+# second param is ID number
+# last param is total image size
 img = aruco.drawMarker(aruco_dict, 2, 700)
 cv2.imwrite("test_marker.jpg", img)
 
