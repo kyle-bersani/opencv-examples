a1cf26bbdd58d3895c7c728d19e070940feb07c6
Kyle <kyle.elsalhi@outlook.com>
2017-04-29 15:23:20 -0700

    Added an example of detecting an Aruco gridboard and estimating board posture.

---
+++ PostureEstimation/EstimatePostureWithArucoGridboard.py
@@ -0,0 +1,87 @@
+# The following code is used to watch a video stream, detect Aruco markers, and use
+# a set of markers to determine the posture of the camera in relation to the plane
+# of markers.
+#
+# Assumes that all markers are on the same plane, for example on the same piece of paper
+#
+# Requires camera calibration (see the rest of the project for example calibration)
+
+import numpy
+import cv2
+import cv2.aruco as aruco
+import os
+import pickle
+
+# Check for camera calibration data
+if not os.path.exists('./calibration.pckl'):
+    print("You need to calibrate the camera you'll be using. See calibration project directory for details.")
+    exit()
+else:
+    f = open('calibration.pckl', 'rb')
+    (cameraMatrix, distCoeffs, _, _) = pickle.load(f)
+    f.close()
+    if cameraMatrix is None or distCoeffs is None:
+        print("Calibration issue. Remove ./calibration.pckl and recalibrate your camera with CalibrateCamera.py.")
+        exit()
+
+# Constant parameters used in Aruco methods
+ARUCO_PARAMETERS = aruco.DetectorParameters_create()
+ARUCO_DICT = aruco.Dictionary_get(aruco.DICT_5X5_50)
+
+# Create grid board object we're using in our stream
+board = aruco.GridBoard_create(
+        markersX=5,
+        markersY=7,
+        markerLength=0.04,
+        markerSeparation=0.01,
+        dictionary=ARUCO_DICT)
+
+# Create vectors we'll be using for rotations and translations for postures
+rvecs, tvecs = None, None
+
+cam = cv2.VideoCapture('gridboard-test-at-kyles-desk2.mp4')
+
+while(cam.isOpened()):
+    # Capturing each frame of our video stream
+    ret, QueryImg = cam.read()
+    if ret == True:
+        # grayscale image
+        gray = cv2.cvtColor(QueryImg, cv2.COLOR_BGR2GRAY)
+    
+        # Detect Aruco markers
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, ARUCO_DICT, parameters=ARUCO_PARAMETERS)
+  
+        # Refine detected markers
+        # Eliminates markers not part of our board, adds missing markers to the board
+        corners, ids, rejectedImgPoints, recoveredIds = aruco.refineDetectedMarkers(
+                image = gray,
+                board = board,
+                detectedCorners = corners,
+                detectedIds = ids,
+                rejectedCorners = rejectedImgPoints,
+                cameraMatrix = cameraMatrix,
+                distCoeffs = distCoeffs)   
+
+        ###########################################################################
+        # TODO: Add validation here to reject IDs/corners not part of a gridboard #
+        ###########################################################################
+
+        # Outline all of the markers detected in our image
+        QueryImg = aruco.drawDetectedMarkers(QueryImg, corners, borderColor=(0, 0, 255))
+
+        # Require 15 markers before drawing axis
+        if ids is not None and len(ids) > 15:
+            # Estimate the posture of the gridboard, which is a construction of 3D space based on the 2D video 
+            pose, rvec, tvec = aruco.estimatePoseBoard(corners, ids, board, cameraMatrix, distCoeffs)
+            if pose:
+                # Draw the camera posture calculated from the gridboard
+                QueryImg = aruco.drawAxis(QueryImg, cameraMatrix, distCoeffs, rvec, tvec, 0.3)
+            
+        # Display our image
+        cv2.imshow('QueryImage', QueryImg)
+
+    # Exit at the end of the video on the 'q' keypress
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv2.destroyAllWindows()
---
+++ PostureEstimation/README.md
@@ -0,0 +1,16 @@
+## Overview
+
+The code in this project is an example and test of using Aruco marker and Gridboard detection in a video stream, then creating board posture (an estimation of 3D space as constructed from a 2D video)
+
+Camera calibration is required, for an example of that check the calibration examplese in this repository.
+
+## Systems
+
+This code in this project was tested on the following systems:
+ - Linux Debian dist
+   - Linux Mint 18.1 Cinnamon 64-bit
+   - Python 2.7 with OpenCV 3.2.0
+ - MacBook Air
+   - macOS Sierra 10.12.4
+   - Python 2.7 with OpenCV 3.2.0
+ 
