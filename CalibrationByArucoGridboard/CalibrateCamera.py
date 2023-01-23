14f9782ba81f570b52ff3f4d4dbba95ac60167a5
joke678 <96171054+joke678@users.noreply.github.com>
2023-01-23 12:52:21 +0100

    Update CalibrateCamera.py

--- CalibrationByArucoGridboard/CalibrateCamera.py
+++ CalibrationByArucoGridboard/CalibrateCamera.py
@@ -3,12 +3,11 @@ import cv2
 import cv2.aruco as aruco
 import pickle
 
-aruco_dict = aruco.Dictionary_get(aruco.DICT_5X5_1000)
-
+aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_5X5_1000)
+markersX=5
+markersY=7
 # Creating a theoretical board we'll use to calculate marker positions
-board = aruco.GridBoard_create(
-    markersX=5,
-    markersY=7,
+board = aruco.GridBoard([markersX,markersY],
     markerLength=0.04,
     markerSeparation=0.01,
     dictionary=aruco_dict)
@@ -25,7 +24,7 @@ while(cam.isOpened()):
     if ret == True:
         # grayscale image
         gray = cv2.cvtColor(QueryImg, cv2.COLOR_BGR2GRAY)
-        parameters = aruco.DetectorParameters_create()
+        parameters = aruco.DetectorParameters()
     
         # Detect Aruco markers
         corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
